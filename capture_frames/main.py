1c946afb12cca9724cdd1c5d9a90527d53f552a2
Deepraj Das <mail30deeprajdas@gmail.com>
2024-04-25 02:20:51 -0700

    main

---
+++ capture_frames/main.py
@@ -0,0 +1,21 @@
+import cv2
+
+def capture_frames():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        print(frame.tobytes()) # Here it will print the bytes of single frame
+        # cv2.imshow('Frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    capture_frames()
